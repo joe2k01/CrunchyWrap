38eb610b91784947e0dc23fba5115c113b739611
Giuseppe Barillari <joe2k01dev@gmail.com>
2019-09-02 13:36:27 +0200

    Initial commit
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ app/src/main/java/io/github/joe2k01/animegratis/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.joe2k01.animegratis
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
