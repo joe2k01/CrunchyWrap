7d2cc8eed24bf771e3565b019c09d42d6f6c2bb9
Giuseppe Barillari <joe2k01dev@gmail.com>
2019-09-03 10:49:16 +0200

    Extract the newest anime information from returned array
     * Also initially populate a listview
    
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- app/src/main/java/io/github/joe2k01/animegratis/NewestFragment.kt
+++ app/src/main/java/io/github/joe2k01/animegratis/NewestFragment.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_newest.*
+import org.json.JSONObject
 
 /**
  * A simple [Fragment] subclass.
@@ -24,6 +27,21 @@ class NewestFragment : Fragment() {
 
         val apiCalls = ApiCalls()
         val newest = apiCalls.getNewest()
+
+        val titles = arrayOfNulls<String>(10)
+        val image = arrayOfNulls<String>(10)
+        val seriesId = arrayOfNulls<String>(10)
+        for (x in newest.indices) {
+            val json = JSONObject(newest[x]!!)
+            titles[x] = json.getString("name")
+            seriesId[x] = json.getString("series_id")
+            val portrait = JSONObject(json.getString("portrait_image"))
+            image[x] = portrait.getString("medium_url")
+        }
+
+        val arrayAdapter =
+            ArrayAdapter(activity!!.baseContext, android.R.layout.simple_list_item_1, titles)
+        listView.adapter = arrayAdapter
     }
 
 }
