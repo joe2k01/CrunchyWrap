007d93286f01e3a3b78fe738291274dc6aacf9b1
Giuseppe Barillari <joe2k01dev@gmail.com>
2019-09-04 22:35:12 +0200

    Implement a simple play menu
     * Play/Pause
     * Fast forward/backward
    
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- app/src/main/java/io/github/joe2k01/animegratis/StreamingActivity.kt
+++ app/src/main/java/io/github/joe2k01/animegratis/StreamingActivity.kt
@@ -4,10 +4,15 @@ import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.MediaController
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_streaming.*
 
 class StreamingActivity : AppCompatActivity() {
+
+    private lateinit var mediaController: MediaController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,40 @@ class StreamingActivity : AppCompatActivity() {
         val url = ApiCalls().getStreamingLink(intent.getStringExtra("id")!!)
         videoView.setVideoURI(Uri.parse(url))
         videoView.start()
+
+        videoView.setOnPreparedListener {
+            progress.max = videoView.duration
+        }
+
+        videoView.setOnClickListener {
+            if (controls.isVisible)
+                controls.visibility = View.GONE
+            else {
+                progress.progress = videoView.currentPosition
+                controls.visibility = View.VISIBLE
+            }
+        }
+
+        progress.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+            override fun onProgressChanged(p0: SeekBar?, position: Int, user: Boolean) {
+                if (user)
+                    videoView.seekTo(position)
+            }
+        })
+
+        play.setOnClickListener {
+            if (videoView.isPlaying) {
+                play.setImageDrawable(resources.getDrawable(R.drawable.ic_play))
+                videoView.pause()
+            } else {
+                play.setImageDrawable(resources.getDrawable(R.drawable.ic_pause))
+                videoView.start()
+            }
+        }
     }
 
     override fun onStart() {
